6b6b133f477fc4757bfe160d63a7bb69127aaca4
Jaheem-D-Prevost <jaheem.prevost.cohort2211@gmail.com>
2023-01-04 01:50:03 -0400

    Incoporate email rendering functionality

---
+++ assets/js/emailData.js
@@ -0,0 +1,67 @@
+const emailData = [
+  {
+    sender: "Dad",
+    subject: "I Have Something I Need To Tell You. When are you going to come visit again?",
+    date: "18/01/23",
+    email: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, eligendi molestias iusto debitis quo iste inventore! Commodi laborum quidem ipsam.",
+    id: "VSKZ"
+  },
+  {
+    sender: "Norman",
+    subject: "Turns out I'm 25% Alien bro, I always knew something was off about my grandma.",
+    date: "01/01/23",
+    email: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, eligendi molestias iusto debitis quo iste inventore! Commodi laborum quidem ipsam.",
+    id: "SMV2"
+  },
+  {
+    sender: "Wendy",
+    subject: "Ok so I know you probably don't want to speak to me right now, but I promise I was always going to tell you eventually.",
+    date: "25/12/22",
+    email: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, eligendi molestias iusto debitis quo iste inventore! Commodi laborum quidem ipsam.",
+    id: "QQH8"
+  },
+  {
+    sender: "Derek",
+    subject: "So Ally told me you took the news pretty well. So let me just come out and say it; we're all aliens.",
+    date: "19/12/22",
+    email: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, eligendi molestias iusto debitis quo iste inventore! Commodi laborum quidem ipsam.",
+    id: "R6FP"
+  },
+  {
+    sender: "Allison",
+    subject: "Please don't hate me but you've heard the news recently, yeah?",
+    date: "24/11/22",
+    email: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, eligendi molestias iusto debitis quo iste inventore! Commodi laborum quidem ipsam.",
+    id: "COEH"
+  },
+  {
+    sender: "World News",
+    subject: "They've Achieved This By Disguising Themselves As Humans, They Could Be Anyone. Some Of You Reading This Could Even Be Part-Alien.",
+    date: "10/10/22",
+    email: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, eligendi molestias iusto debitis quo iste inventore! Commodi laborum quidem ipsam.",
+    id: "ZRPL"
+  },
+  {
+    sender: "World News",
+    subject: "It Appears They've Been Here For Centuries In Plain Sight. Mingling With The Human Race!",
+    date: "29/09/22",
+    email: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, eligendi molestias iusto debitis quo iste inventore! Commodi laborum quidem ipsam.",
+    id: "YVBQ"
+  },
+  {
+    sender: "World News",
+    subject: "Alien Lifeforms Are Walking Among Us! They Claim To Be Non-Hostile.",
+    date: "01/09/22",
+    email: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, eligendi molestias iusto debitis quo iste inventore! Commodi laborum quidem ipsam.",
+    id: "7B9H"
+  },
+  {
+    sender: "World News",
+    subject: "Warning To All Citizens! Imminent Threat Approaching From Beyond The Veil.",
+    date: "03/08/22",
+    email: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, eligendi molestias iusto debitis quo iste inventore! Commodi laborum quidem ipsam.",
+    id: "JT89"
+  }          
+];
+
+export default emailData;
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,19 @@
+import emailData from './emailData.js';
+
 const navButton = document.querySelector('.nav-btn');
 const overlay = document.querySelector('.overlay');
 const MobileNavigation = document.querySelector('.mobile-navigation');
 const searchBar = document.querySelector('.search-bar');
 const listItems = document.querySelectorAll('.list-item');
+const header = document.querySelector('.header');
+const emailSection = document.querySelector('.emails'); 
+const individualEmail = document.querySelector('.individual-email');
+const backBtn = document.querySelector('.back-btn'); 
+const emailBody = document.querySelector('.email-body'); 
 
+
+/*Adds and removes special utility 
+classes needed to open the navigation menu.*/
 const openMenu = function() {
   overlay.classList.add('fade-in');
   MobileNavigation.classList.add('active');
@@ -11,15 +21,19 @@ const openMenu = function() {
   MobileNavigation.classList.remove('inactive');
 }; 
 
+//Adds event listener to navigation button.
 navButton.addEventListener('click', openMenu);
 
+ 
+/*Adds and removes special utility 
+classes needed to close the navigation menu.*/
 const closeMenu = function() {
   overlay.classList.remove('fade-in');
   MobileNavigation.classList.remove('active');
   overlay.classList.add('fade-out');
   MobileNavigation.classList.add('inactive');
 }
-
+//Adds event listener to background overlay.
 overlay.addEventListener('click', closeMenu);
 
 const filterInbox = function() {
@@ -36,4 +50,62 @@ const filterInbox = function() {
   });
 };
 
+//Adds a click event listener to the search input.
 searchBar.addEventListener('input', filterInbox);
+ 
+//Renders an html template for each object in the emailData array.
+for (let i = 0; i < listItems.length; i++) {
+  let htmlTemplate = `
+  <div class="email ${emailData[i].id}">
+    <h2 class="sender">${emailData[i].sender}</h2>
+    <p class="subject">${emailData[i]['subject'].slice(0, 19)}...</p>
+    <p class="date">${emailData[i].date}</p>
+    <p class="lead">${emailData[i]['email'].slice(0, 27)}...</p>
+  </div>
+  `;
+  listItems[i].innerHTML = htmlTemplate;
+}
+
+//
+const emails = document.querySelectorAll('.email');
+emails.forEach(email => {
+  const renderEmail = () => {
+    const id = Array.from(email.classList)[1];
+
+    const emailObj = emailData.find(obj => obj.id === id);
+ 
+    individualEmail.style.display = 'block';
+
+    const emailTemplate = `
+      <h2 class="email-heading">${emailObj.subject}</h2>
+
+      <button class="favorite-btn">
+        <i class="fa-regular fa-star"></i>
+      </button>
+
+      <div class="sender-content">
+          <div class="sender-info">
+            <i class="fa-regular fa-circle-user sender-icon"></i>
+
+            <p class="sender-name">${emailObj.sender}</p>
+          </div>
+
+          <div class="sender-options"> 
+            <i class="fa-solid fa-reply reply"></i>
+
+            <i class="fa-solid fa-ellipsis-vertical more-settings"></i>
+          </div>
+      </div>
+
+      <p class="email-content">${emailObj.email}</p> 
+    `;
+
+    emailBody.innerHTML = emailTemplate;
+  };
+
+  email.addEventListener('click', renderEmail);
+});
+
+backBtn.addEventListener('click', () => {
+  individualEmail.style.display = 'none';
+});
